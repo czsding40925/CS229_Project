572b07d10f785bf3f5f8022fc72d82554253c53c
czsding40925 <connording40925@gmail.com>
2023-11-30 10:19:26 -0800

    added processing script

--- Code/Further_processing.m
+++ Code/Further_processing.m
@@ -3,8 +3,27 @@
 data_summaries = {'data_summary_09_12.mat','data_summary_09_14.mat','data_summary_09_15.mat'};
 F1_scores = zeros(5,3);
 Mean_accuracy = zeros(5,3);
+
 for i = 1:3
     this_data = load(data_summaries{i});
-    F1_scores(:,i) = this_data.F1;
-    Mean_accuracy = this_data.Accuracy;
-end 
+    
+    % Get the field names of the loaded data
+    data_fieldnames = fieldnames(this_data);
+    
+    % Assuming there's only one field per loaded file
+    if length(data_fieldnames) == 1
+        struct_name = data_fieldnames{1};
+        
+        % Access the fields from the struct
+        F1_scores(:,i) = this_data.(struct_name).F1;
+        Mean_accuracy(:,i) = this_data.(struct_name).Accuracy;
+    else
+        error('Unexpected number of fields in the loaded data');
+    end
+end
+
+% Average of F1_scores across columns
+average_F1_scores = mean(F1_scores,2);
+
+% Average of Mean_accuracy across columns
+average_Mean_accuracy = mean(Mean_accuracy,2);
